f227a009698013361fc224f7fbcf2c87ff2a758c
eyemono-moe <ysdr83@gmail.com>
2024-09-30 22:50:32 +0900

    fix: レイアウトシフトを抑制

--- src/features/ShortText/components/Reactions.tsx
+++ src/features/ShortText/components/Reactions.tsx
@@ -55,44 +55,46 @@ const Reactions: Component<{
   });
 
   return (
-    <div class="flex flex-wrap gap-1">
-      <For each={parsedReactions()}>
-        {(reaction) => (
+    <Show when={(reactions.data?.length ?? 0) > 0}>
+      <div class="flex flex-wrap gap-1">
+        <For each={parsedReactions()}>
+          {(reaction) => (
+            <button
+              class="b-1 b-zinc-2 flex appearance-none items-center gap-1 rounded bg-transparent p-0.5"
+              type="button"
+            >
+              <div class="flex h-5 items-center justify-center">
+                <Show
+                  when={reaction.content.type === "emoji" && reaction.content}
+                  fallback={
+                    <span class="h-5 truncate leading-5">
+                      {reaction.content.value}
+                    </span>
+                  }
+                >
+                  {(emoji) => (
+                    <img
+                      src={emoji().src}
+                      class="inline-block h-full w-auto"
+                      alt={emoji().value}
+                    />
+                  )}
+                </Show>
+              </div>
+              <span class="h-5 text-zinc-5 leading-5">{reaction.count}</span>
+            </button>
+          )}
+        </For>
+        <Show when={(reactions.data?.length ?? 0) > 0}>
           <button
             class="b-1 b-zinc-2 flex appearance-none items-center gap-1 rounded bg-transparent p-0.5"
             type="button"
           >
-            <div class="flex h-5 items-center justify-center">
-              <Show
-                when={reaction.content.type === "emoji" && reaction.content}
-                fallback={
-                  <span class="h-5 truncate leading-5">
-                    {reaction.content.value}
-                  </span>
-                }
-              >
-                {(emoji) => (
-                  <img
-                    src={emoji().src}
-                    class="inline-block h-full w-auto"
-                    alt={emoji().value}
-                  />
-                )}
-              </Show>
-            </div>
-            <span class="h-5 text-zinc-5 leading-5">{reaction.count}</span>
+            <div class="i-material-symbols:add-rounded c-zinc-5 aspect-square h-5 w-auto" />
           </button>
-        )}
-      </For>
-      <Show when={(reactions.data?.length ?? 0) > 0}>
-        <button
-          class="b-1 b-zinc-2 flex appearance-none items-center gap-1 rounded bg-transparent p-0.5"
-          type="button"
-        >
-          <div class="i-material-symbols:add-rounded c-zinc-5 aspect-square h-5 w-auto" />
-        </button>
-      </Show>
-    </div>
+        </Show>
+      </div>
+    </Show>
   );
 };
 
