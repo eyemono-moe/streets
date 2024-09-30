f827e52c63b5c6c3538381ddbbb02a0562673b5f
eyemono-moe <ysdr83@gmail.com>
2024-09-30 22:55:20 +0900

    feat: テキストの埋め込みをsuspenseするように

--- src/features/ShortText/components/ShortTextContent.tsx
+++ src/features/ShortText/components/ShortTextContent.tsx
@@ -1,4 +1,11 @@
-import { type Component, For, Match, Show, Switch } from "solid-js";
+import {
+  type Component,
+  For,
+  Match,
+  Show,
+  SuspenseList,
+  Switch,
+} from "solid-js";
 import type {
   ImageContent,
   LinkContent,
@@ -16,64 +23,66 @@ const ShortTextContent: Component<{
   showLinkEmbeds?: boolean;
 }> = (props) => {
   return (
-    <For each={props.contents}>
-      {(content) => (
-        <Switch>
-          <Match when={content.type === "text"}>
-            <span class="break-anywhere whitespace-pre-wrap">
-              {(content as TextContent).content}
-            </span>
-          </Match>
-          <Match when={content.type === "image"}>
-            {/* TODO: 画像の拡大表示 */}
-            <a
-              href={(content as ImageContent).src}
-              target="_blank"
-              rel="noopener noreferrer"
-              class="block w-fit"
-            >
-              {/* TODO: display blurhash */}
-              <img
-                class="b-1 b-zinc-2 h-full max-h-50vh w-auto max-w-full rounded object-contain"
-                src={(content as ImageContent).src}
-                alt={(content as ImageContent).alt ?? ""}
-                width={(content as ImageContent).size?.width}
-                height={(content as ImageContent).size?.height}
-                loading="lazy"
-              />
-            </a>
-          </Match>
-          <Match when={content.type === "link"}>
-            <Show
-              when={props.showLinkEmbeds}
-              fallback={
-                <a
+    <SuspenseList revealOrder="forwards">
+      <For each={props.contents}>
+        {(content) => (
+          <Switch>
+            <Match when={content.type === "text"}>
+              <span class="break-anywhere whitespace-pre-wrap">
+                {(content as TextContent).content}
+              </span>
+            </Match>
+            <Match when={content.type === "image"}>
+              {/* TODO: 画像の拡大表示 */}
+              <a
+                href={(content as ImageContent).src}
+                target="_blank"
+                rel="noopener noreferrer"
+                class="block w-fit"
+              >
+                {/* TODO: display blurhash */}
+                <img
+                  class="b-1 b-zinc-2 h-full max-h-50vh w-auto max-w-full rounded object-contain"
+                  src={(content as ImageContent).src}
+                  alt={(content as ImageContent).alt ?? ""}
+                  width={(content as ImageContent).size?.width}
+                  height={(content as ImageContent).size?.height}
+                  loading="lazy"
+                />
+              </a>
+            </Match>
+            <Match when={content.type === "link"}>
+              <Show
+                when={props.showLinkEmbeds}
+                fallback={
+                  <a
+                    href={(content as LinkContent).href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="c-blue-5 visited:c-violet-7 break-anywhere whitespace-pre-wrap underline"
+                  >
+                    {(content as LinkContent).content}
+                  </a>
+                }
+              >
+                <Link
                   href={(content as LinkContent).href}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  class="c-blue-5 visited:c-violet-7 break-anywhere whitespace-pre-wrap underline"
-                >
-                  {(content as LinkContent).content}
-                </a>
-              }
-            >
-              <Link
-                href={(content as LinkContent).href}
-                content={(content as LinkContent).content}
-              />
-            </Show>
-          </Match>
-          <Match when={content.type === "mention"}>
-            <EmbedUser pubkey={(content as MentionContent).pubkey} />
-          </Match>
-          <Match when={content.type === "quote"}>
-            <div class="b-1 b-zinc-2 overflow-hidden rounded p-1">
-              <Quote id={(content as QuoteContent).id} />
-            </div>
-          </Match>
-        </Switch>
-      )}
-    </For>
+                  content={(content as LinkContent).content}
+                />
+              </Show>
+            </Match>
+            <Match when={content.type === "mention"}>
+              <EmbedUser pubkey={(content as MentionContent).pubkey} />
+            </Match>
+            <Match when={content.type === "quote"}>
+              <div class="b-1 b-zinc-2 overflow-hidden rounded p-1">
+                <Quote id={(content as QuoteContent).id} />
+              </div>
+            </Match>
+          </Switch>
+        )}
+      </For>
+    </SuspenseList>
   );
 };
 
