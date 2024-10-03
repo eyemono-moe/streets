7ae2df97d7e7d35a31021694050ac0ee1c8ae07e
eyemono-moe <ysdr83@gmail.com>
2024-10-04 06:10:58 +0900

    remove log

--- src/features/ShortText/components/Reply.tsx
+++ src/features/ShortText/components/Reply.tsx
@@ -4,7 +4,6 @@ import Text from "./Text";
 const Reply: Component<{
   id?: string;
 }> = (props) => {
-  console.log("Reply", props.id);
   return <Text id={props.id} small isReplyTarget showReply />;
 };
 
