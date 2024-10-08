158ed419e3c90522a221ac04c6373cae7b69d5d2
eyemono-moe <ysdr83@gmail.com>
2024-10-09 04:05:58 +0900

    feat: add solid devtools

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { render } from "solid-js/web";
 import App from "./App";
 import "@unocss/reset/tailwind-compat.css";
 import "virtual:uno.css";
+import "solid-devtools";
 
 // biome-ignore lint/style/noNonNullAssertion: div#root in index.html
 const root = document.getElementById("root")!;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import devtools from "solid-devtools/vite";
 import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [UnoCSS(), solid()],
+  plugins: [
+    devtools({
+      autoname: true,
+      locator: {
+        targetIDE: "vscode",
+      },
+    }),
+    UnoCSS(),
+    solid(),
+  ],
 });
