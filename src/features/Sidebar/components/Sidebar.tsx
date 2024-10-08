a536ffef799237854c768160c02e360cbc2f1092
eyemono-moe <ysdr83@gmail.com>
2024-10-09 03:43:31 +0900

    feat: chore styling

--- src/features/Sidebar/components/Sidebar.tsx
+++ src/features/Sidebar/components/Sidebar.tsx
@@ -1,21 +1,33 @@
 import { Collapsible } from "@kobalte/core/collapsible";
 import type { Component } from "solid-js";
+import Logo from "../../../assets/logo.svg";
 import PostInput from "../../ShortText/components/PostInput";
 
 const Sidebar: Component = () => {
   return (
-    <Collapsible defaultOpen>
+    <Collapsible>
       <div class="flex h-full divide-x">
-        <div class="c-zinc-6 grid grid-rows-[auto_1fr_auto] p-1">
-          <div class="flex flex-col gap-2">
-            <Collapsible.Trigger>
+        <div class="c-zinc-6 grid grid-rows-[auto_1fr_auto]">
+          <div class="flex flex-col">
+            <Collapsible.Trigger class="appearance-none bg-transparent p-1">
               <div class="i-material-symbols:edit-square-outline-rounded aspect-square h-auto w-8" />
             </Collapsible.Trigger>
-            <div class="i-material-symbols:search-rounded aspect-square h-auto w-8" />
+            <button class="appearance-none bg-transparent p-1" type="button">
+              <div class="i-material-symbols:search-rounded aspect-square h-auto w-8" />
+            </button>
           </div>
           <div>{/* TODO: 開いているcolumnへのショートカット */}</div>
-          <div>
-            <div class="i-material-symbols:add-rounded aspect-square h-auto w-8" />
+          <div class="flex flex-col">
+            <button class="appearance-none bg-transparent p-1" type="button">
+              <div class="i-material-symbols:add-rounded aspect-square h-auto w-8" />
+            </button>
+            <button class="appearance-none bg-transparent p-1" type="button">
+              <img
+                src={Logo}
+                alt="strands logo"
+                class="aspect-square h-auto w-8"
+              />
+            </button>
           </div>
         </div>
         <Collapsible.Content>
