0af8dc7146fc3c436310dbf30f104edb97dc73c9
eyemono-moe <ysdr83@gmail.com>
2024-10-10 18:39:18 +0900

    fix: ユーザー行表示でのホバークリックでボタンが反応してしまっていた

--- src/features/User/components/ProfileHoverContent.tsx
+++ src/features/User/components/ProfileHoverContent.tsx
@@ -6,7 +6,10 @@ const ProfileHoverContent: Component<{
   pubkey?: string;
 }> = (props) => {
   return (
-    <HoverCard.Content class="transform-origin-[--kb-hovercard-content-transform-origin] b-1 b-zinc-2 max-h-[min(calc(100vh-32px),360px)] min-h-0 max-w-[min(calc(100vw-32px),360px)] overflow-hidden rounded-2 bg-white shadow-xl">
+    <HoverCard.Content
+      class="transform-origin-[--kb-hovercard-content-transform-origin] b-1 b-zinc-2 max-h-[min(calc(100vh-32px),360px)] min-h-0 max-w-[min(calc(100vw-32px),360px)] overflow-hidden rounded-2 bg-white shadow-xl"
+      onClick={(e) => e.stopPropagation()}
+    >
       <Profile pubkey={props.pubkey} small />
     </HoverCard.Content>
   );
