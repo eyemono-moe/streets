a0828240cdb0b5b4084ddedff15973fc7afd0b52
eyemono-moe <ysdr83@gmail.com>
2024-10-12 02:24:29 +0900

    feat: animate profile card

--- src/features/User/components/ProfileHoverContent.tsx
+++ src/features/User/components/ProfileHoverContent.tsx
@@ -7,7 +7,7 @@ const ProfileHoverContent: Component<{
 }> = (props) => {
   return (
     <HoverCard.Content
-      class="transform-origin-[--kb-hovercard-content-transform-origin] b-1 b-zinc-2 max-h-[min(calc(100vh-32px),360px)] min-h-0 max-w-[min(calc(100vw-32px),360px)] overflow-hidden rounded-2 bg-white shadow-xl"
+      class="transform-origin-[--kb-hovercard-content-transform-origin] b-1 b-zinc-2 max-h-[min(calc(100vh-32px),360px)] min-h-0 max-w-[min(calc(100vw-32px),360px)] animate-duration-100 animate-fade-out overflow-hidden rounded-2 bg-white shadow-xl data-[expanded]:animate-duration-100 data-[expanded]:animate-fade-in"
       onClick={(e: MouseEvent) => e.stopPropagation()}
     >
       <Profile pubkey={props.pubkey} small />
