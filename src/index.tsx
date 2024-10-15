911daaefd1e19cd35506501517b4a1fe1a658788
eyemono-moe <ysdr83@gmail.com>
2024-10-16 04:42:31 +0900

    feat: nostr-loginのパッケージでの利用をやめる

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 /* @refresh reload */
-import { init } from "nostr-login";
 import { render } from "solid-js/web";
 import App from "./App";
 import "@unocss/reset/tailwind-compat.css";
@@ -7,13 +6,7 @@ import "virtual:uno.css";
 import "solid-devtools";
 import "unfonts.css";
 
-const setup = async () => {
-  await init({});
+// biome-ignore lint/style/noNonNullAssertion: div#root in index.html
+const root = document.getElementById("root")!;
 
-  // biome-ignore lint/style/noNonNullAssertion: div#root in index.html
-  const root = document.getElementById("root")!;
-
-  render(() => <App />, root);
-};
-
-setup();
+render(() => <App />, root);
