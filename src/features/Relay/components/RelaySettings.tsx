ec81ecef9b194d3ff66eb8f36d55c185b7c3ae26
eyemono-moe <ysdr83@gmail.com>
2024-10-17 07:32:47 +0900

    todo: load relays from extension

--- src/features/Relay/components/RelaySettings.tsx
+++ src/features/Relay/components/RelaySettings.tsx
@@ -120,6 +120,19 @@ const RelaySettings: Component = () => {
     );
   });
 
+  // TODO: nostr-loginを使用するとgetRelaysが使えないため一旦実装を見送る
+  // see: https://github.com/nostrband/nostr-login/blob/3f65e1e57df6353996996169e643ba9ce1bf1b87/packages/auth/src/modules/Nostr.ts#L81-L84
+  // const [nip07Relays] = createResource(() => useNIP07().getRelays());
+  // const syncWithNIP07 = async () => {
+  //   if (nip07Relays.state !== "ready") return;
+  //   const arr = Object.entries(nip07Relays()).map(([url, { read, write }]) => ({
+  //     url,
+  //     read,
+  //     write,
+  //   }));
+  //   setValues(form, { relays: arr });
+  // };
+
   const handleSave: SubmitHandler<RelayConfigForm> = (input) => {
     const parsed = v.parse(relayConfigFormSchema, input);
     const relayRecord = Object.fromEntries(
@@ -133,85 +146,94 @@ const RelaySettings: Component = () => {
   };
 
   return (
-    <Form onSubmit={handleSave} class="flex flex-col gap-2">
-      <div class="grid grid-cols-[minmax(0,1fr)_auto_auto_auto] gap-x-2">
-        <div class="grid-col-span-3 grid grid-cols-[subgrid]">
-          <div>{t("settings.relay.relayURL")}</div>
-          <div>{t("settings.relay.read")}</div>
-          <div>{t("settings.relay.write")}</div>
+    <>
+      <Form onSubmit={handleSave} class="flex flex-col gap-2">
+        <div class="grid grid-cols-[minmax(0,1fr)_auto_auto_auto] gap-x-2">
+          <div class="grid-col-span-3 grid grid-cols-[subgrid]">
+            <div>{t("settings.relay.relayURL")}</div>
+            <div>{t("settings.relay.read")}</div>
+            <div>{t("settings.relay.write")}</div>
+          </div>
+          <FieldArray name="relays">
+            {(fieldArray) => (
+              <For each={fieldArray.items}>
+                {(_, index) => (
+                  <div class="grid-col-span-4 grid grid-cols-[subgrid] place-items-center items-start">
+                    <Field name={`relays.${index()}.url`}>
+                      {(field, props) => (
+                        <TextField
+                          {...props}
+                          type="text"
+                          placeholder="wss://example.com"
+                          value={field.value}
+                          error={field.error}
+                          required
+                        />
+                      )}
+                    </Field>
+                    <Field name={`relays.${index()}.read`} type="boolean">
+                      {(field, props) => (
+                        <Checkbox
+                          {...props}
+                          checked={field.value}
+                          error={field.error}
+                        />
+                      )}
+                    </Field>
+                    <Field name={`relays.${index()}.write`} type="boolean">
+                      {(field, props) => (
+                        <Checkbox
+                          {...props}
+                          checked={field.value}
+                          error={field.error}
+                        />
+                      )}
+                    </Field>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        remove(form, "relays", { at: index() });
+                      }}
+                      class="c-red-6 appearance-none rounded-full bg-transparent p-1 hover:bg-red-1"
+                    >
+                      <div class="i-material-symbols:delete-outline-rounded aspect-square h-1lh w-auto" />
+                    </button>
+                  </div>
+                )}
+              </For>
+            )}
+          </FieldArray>
         </div>
-        <FieldArray name="relays">
-          {(fieldArray) => (
-            <For each={fieldArray.items}>
-              {(_, index) => (
-                <div class="grid-col-span-4 grid grid-cols-[subgrid] place-items-center items-start">
-                  <Field name={`relays.${index()}.url`}>
-                    {(field, props) => (
-                      <TextField
-                        {...props}
-                        type="text"
-                        placeholder="wss://example.com"
-                        value={field.value}
-                        error={field.error}
-                        required
-                      />
-                    )}
-                  </Field>
-                  <Field name={`relays.${index()}.read`} type="boolean">
-                    {(field, props) => (
-                      <Checkbox
-                        {...props}
-                        checked={field.value}
-                        error={field.error}
-                      />
-                    )}
-                  </Field>
-                  <Field name={`relays.${index()}.write`} type="boolean">
-                    {(field, props) => (
-                      <Checkbox
-                        {...props}
-                        checked={field.value}
-                        error={field.error}
-                      />
-                    )}
-                  </Field>
-                  <button
-                    type="button"
-                    onClick={() => {
-                      remove(form, "relays", { at: index() });
-                    }}
-                    class="c-red-6 appearance-none rounded-full bg-transparent p-1 hover:bg-red-1"
-                  >
-                    <div class="i-material-symbols:delete-outline-rounded aspect-square h-1lh w-auto" />
-                  </button>
-                </div>
-              )}
-            </For>
-          )}
-        </FieldArray>
-      </div>
-      <div class="flex w-full justify-center text-sm">
-        <Button variant="border" onClick={handleAddRelay}>
-          <div class="i-material-symbols:add-rounded aspect-square h-1lh w-auto" />
-          {t("settings.relay.add")}
-        </Button>
-      </div>
-      <div class="flex justify-end gap-1">
-        <Show when={form.dirty}>
-          <Button type="button" onClick={() => reset(form)} variant="border">
-            {t("settings.relay.discard")}
+        <div class="flex w-full justify-center text-sm">
+          <Button variant="border" onClick={handleAddRelay}>
+            <div class="i-material-symbols:add-rounded aspect-square h-1lh w-auto" />
+            {t("settings.relay.add")}
           </Button>
-        </Show>
-        <Button
-          type="submit"
-          disabled={
-            !form.dirty || form.invalid || form.submitting || form.validating
-          }
-        >
-          {t("settings.relay.save")}
-        </Button>
-      </div>
-    </Form>
+        </div>
+        <div class="flex justify-end gap-1">
+          {/* <Show when={nip07Relays()}>
+            <div class="mr-auto">
+              <Button type="button" onClick={syncWithNIP07} variant="border">
+                {t("settings.relay.loadFromExtension")}
+              </Button>
+            </div>
+          </Show> */}
+          <Show when={form.dirty}>
+            <Button type="button" onClick={() => reset(form)} variant="border">
+              {t("settings.relay.discard")}
+            </Button>
+          </Show>
+          <Button
+            type="submit"
+            disabled={
+              !form.dirty || form.invalid || form.submitting || form.validating
+            }
+          >
+            {t("settings.relay.save")}
+          </Button>
+        </div>
+      </Form>
+    </>
   );
 };
 
