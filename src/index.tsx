140f9d1afed770ebce44ee669a75eac96f0fc142
eyemono-moe <ysdr83@gmail.com>
2024-10-17 08:13:03 +0900

    feat: setup sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/solid";
 /* @refresh reload */
 import { render } from "solid-js/web";
 import App from "./App";
@@ -5,6 +6,21 @@ import "@unocss/reset/tailwind-compat.css";
 import "virtual:uno.css";
 import "solid-devtools";
 import "unfonts.css";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost"],
+  // Session Replay
+  replaysSessionSampleRate: import.meta.env.DEV ? 1.0 : 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 // biome-ignore lint/style/noNonNullAssertion: div#root in index.html
 const root = document.getElementById("root")!;
