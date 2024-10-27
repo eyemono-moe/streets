850c4f942f8489ec2a1662f6adaf6513c2e02fbb
eyemono-moe <ysdr83@gmail.com>
2024-10-27 16:51:38 +0900

    fix: scroll to top buttonのz-indexを修正

--- src/features/Column/components/Column.tsx
+++ src/features/Column/components/Column.tsx
@@ -39,45 +39,47 @@ const Column: Component<{
       </div>
       <ColumnContent content={props.column.content} showHeader />
       <Show when={presence.isMounted()}>
-        <button
-          type="button"
-          class="absolute inset-0 bg-op-80! bg-secondary data-[expanded='false']:animate-duration-200 data-[expanded='false']:animate-fade-out data-[expanded='true']:animate-duration-200 data-[expanded='true']:animate-fade-in"
-          onClick={closeTempColumn}
-          data-expanded={presence.isVisible()}
-        />
-        <div
-          class="absolute inset-0 top-10 transition-transform duration-200 data-[expanded='false']:translate-y-100% data-[expanded='true']:translate-y-0"
-          data-expanded={presence.isVisible()}
-        >
-          <div class="grid h-full grid-rows-[auto_minmax(0,1fr)] divide-y overflow-hidden rounded-t-2 bg-primary">
-            <div class="flex items-center gap-1 p-1">
-              <button
-                type="button"
-                class="c-secondary appearance-none rounded-full bg-transparent p-1 enabled:hover:bg-alpha-hover enabled:hover:bg-opacity-50"
-                onClick={backOrCloseTempColumn}
-              >
-                <div class="i-material-symbols:chevron-left-rounded aspect-square h-6 w-auto" />
-              </button>
-              <button
-                type="button"
-                class="c-secondary ml-auto appearance-none rounded-full bg-transparent p-1 enabled:hover:bg-alpha-hover enabled:hover:bg-opacity-50"
-                onClick={transferTempColumn}
-                title={t("column.openInNextColumn")}
-              >
-                <div class="i-material-symbols:open-in-new-rounded aspect-square h-6 w-auto" />
-              </button>
-              <button
-                type="button"
-                class="c-secondary appearance-none rounded-full bg-transparent p-1 enabled:hover:bg-alpha-hover enabled:hover:bg-opacity-50"
-                onClick={closeTempColumn}
-              >
-                <div class="i-material-symbols:close-rounded aspect-square h-6 w-auto" />
-              </button>
+        <div class="z-1">
+          <button
+            type="button"
+            class="absolute inset-0 bg-op-80! bg-secondary data-[expanded='false']:animate-duration-200 data-[expanded='false']:animate-fade-out data-[expanded='true']:animate-duration-200 data-[expanded='true']:animate-fade-in"
+            onClick={closeTempColumn}
+            data-expanded={presence.isVisible()}
+          />
+          <div
+            class="absolute inset-0 top-10 transition-transform duration-200 data-[expanded='false']:translate-y-100% data-[expanded='true']:translate-y-0"
+            data-expanded={presence.isVisible()}
+          >
+            <div class="grid h-full grid-rows-[auto_minmax(0,1fr)] divide-y overflow-hidden rounded-t-2 bg-primary">
+              <div class="flex items-center gap-1 p-1">
+                <button
+                  type="button"
+                  class="c-secondary appearance-none rounded-full bg-transparent p-1 enabled:hover:bg-alpha-hover enabled:hover:bg-opacity-50"
+                  onClick={backOrCloseTempColumn}
+                >
+                  <div class="i-material-symbols:chevron-left-rounded aspect-square h-6 w-auto" />
+                </button>
+                <button
+                  type="button"
+                  class="c-secondary ml-auto appearance-none rounded-full bg-transparent p-1 enabled:hover:bg-alpha-hover enabled:hover:bg-opacity-50"
+                  onClick={transferTempColumn}
+                  title={t("column.openInNextColumn")}
+                >
+                  <div class="i-material-symbols:open-in-new-rounded aspect-square h-6 w-auto" />
+                </button>
+                <button
+                  type="button"
+                  class="c-secondary appearance-none rounded-full bg-transparent p-1 enabled:hover:bg-alpha-hover enabled:hover:bg-opacity-50"
+                  onClick={closeTempColumn}
+                >
+                  <div class="i-material-symbols:close-rounded aspect-square h-6 w-auto" />
+                </button>
+              </div>
+              <Show when={props.column.tempContent}>
+                {/* biome-ignore lint/style/noNonNullAssertion: Show when props.column.tempContent is not null */}
+                <ColumnContent content={props.column.tempContent!} />
+              </Show>
             </div>
-            <Show when={props.column.tempContent}>
-              {/* biome-ignore lint/style/noNonNullAssertion: Show when props.column.tempContent is not null */}
-              <ColumnContent content={props.column.tempContent!} />
-            </Show>
           </div>
         </div>
       </Show>
