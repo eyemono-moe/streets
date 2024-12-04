081c54e9e4ec1a19641366c73cb8f4e377a9dd1d
eyemono-moe <ysdr83@gmail.com>
2024-12-05 05:33:25 +0900

    feat: add internationalization support for Nostr login titles and descriptions

--- src/context/me.tsx
+++ src/context/me.tsx
@@ -10,6 +10,7 @@ import {
   onMount,
   useContext,
 } from "solid-js";
+import { useI18n } from "../i18n";
 import { useNIP07 } from "../shared/libs/useNIP07";
 
 type MeState = {
@@ -25,14 +26,16 @@ const MeContext = createContext<[state: MeState]>([
 ]);
 
 export const MeProvider: ParentComponent = (props) => {
+  const t = useI18n();
+
   const [myPubkey, setMyPubkey] = createSignal<string | undefined>();
 
   // nip07の読み込み後にnostr loginを初期化しないとアカウント切り替え画面が表示されてしまう
   onMount(async () => {
     const n = await waitNostr(1000);
     await initNostrLogin({
-      title: "test",
-      description: "test",
+      title: t("nostrLogin.title"),
+      description: t("nostrLogin.description"),
     });
     // nip07が見つからなかった時はwelcome screenを表示する
     if (n === undefined) {
