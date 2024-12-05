08f8a5202870d9b0b361719aa9aa289ff1215860
eyemono-moe <ysdr83@gmail.com>
2024-12-05 18:07:34 +0900

    fix: 投稿画面のスタイル修正

--- src/routes/post.tsx
+++ src/routes/post.tsx
@@ -2,23 +2,30 @@ import { useNavigate } from "@solidjs/router";
 import { type Component, Show } from "solid-js";
 import { useDeck } from "../features/Column/context/deck";
 import PostInput from "../features/CreatePost/components/PostInput";
+import { useI18n } from "../i18n";
 
 const Post: Component = () => {
+  const t = useI18n();
+
   const [, , layout] = useDeck();
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div class="grid h-full grid-rows-[auto_minmax(0,1fr)]">
       <Show when={layout() === "vertical"}>
-        <button
-          type="button"
-          class="appearance-none bg-transparent"
-          onClick={() => navigate("/")}
-        >
-          <div class="i-material-symbols:close-rounded aspect-square h-1lh w-auto" />
-        </button>
+        <div class="flex">
+          <button
+            type="button"
+            class="ml-auto appearance-none bg-transparent p-1"
+            onClick={() => navigate("/")}
+          >
+            {t("postInput.cancel")}
+          </button>
+        </div>
       </Show>
-      <PostInput />
+      <div class="overflow-y-auto">
+        <PostInput />
+      </div>
     </div>
   );
 };
