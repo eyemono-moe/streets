996ddf47c3fdec3cdf609cf42a8ded3110461843
eyemono-moe <ysdr83@gmail.com>
2024-12-11 05:57:55 +0900

    fix: switchのサイズを絶対値で指定

--- src/shared/components/UI/Switch.tsx
+++ src/shared/components/UI/Switch.tsx
@@ -16,8 +16,8 @@ export function Switch(props: CheckboxProps) {
     >
       <KSwitch.Input class="peer" />
       <KSwitch.Label>{props.label}</KSwitch.Label>
-      <KSwitch.Control class="b-1 data-[checked]:b-accent inline-flex h-[calc(1lh+4px)] w-[calc(2lh+4px)] shrink-0 not-[[data-disabled]]:cursor-pointer items-center rounded-full not-[[data-checked]]:bg-secondary p-0.5 outline-2 outline-accent outline-offset-2 transition-[background-color]-100 peer-focus-visible:outline data-[checked]:bg-accent-primary">
-        <KSwitch.Thumb class="aspect-square h-1lh w-auto rounded-full bg-white transition-transform-100 data-[checked]:translate-x-[calc(100%-2px)]" />
+      <KSwitch.Control class="b-1 data-[checked]:b-accent inline-flex h-24px w-44px shrink-0 not-[[data-disabled]]:cursor-pointer items-center rounded-full not-[[data-checked]]:bg-secondary p-0.5 outline-2 outline-accent outline-offset-2 transition-[background-color]-100 peer-focus-visible:outline data-[checked]:bg-accent-primary">
+        <KSwitch.Thumb class="aspect-square h-20px w-auto rounded-full bg-white transition-transform-100 data-[checked]:translate-x-[calc(100%-1px)]" />
       </KSwitch.Control>
     </KSwitch>
   );
