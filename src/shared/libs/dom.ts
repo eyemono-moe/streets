4002da47f6d0e62a3d9d5ef48deb684d233c082f
eyemono-moe <ysdr83@gmail.com>
2024-12-14 08:20:35 +0900

    feat: add autocomplete

--- src/shared/libs/dom.ts
+++ src/shared/libs/dom.ts
@@ -1,3 +1,5 @@
+import { insertTextIntoField } from "text-field-edit";
+
 // biome-ignore lint/suspicious/noExplicitAny: any is used for type checking
 const isElement = (el: any): el is HTMLElement => {
   return (
@@ -12,4 +14,28 @@ export const contains = (parent: ContainsTarget, child: ContainsTarget) => {
   if (!parent || !child) return false;
   if (!isElement(parent) || !isElement(child)) return false;
   return parent === child || parent.contains(child);
+};
+
+export const useInsertText = (
+  target:
+    | (HTMLInputElement | HTMLTextAreaElement)
+    | (() => HTMLInputElement | HTMLTextAreaElement | undefined),
+) => {
+  const insertText = (
+    text: string,
+    range?: { start: number; end: number } | undefined,
+  ) => {
+    const el = typeof target === "function" ? target() : target;
+    if (!el) return;
+
+    if (range) {
+      el.setSelectionRange(range.start, range.end);
+    }
+
+    // Windowsでの改行コードをLFに統一
+    const normalizedText = text.replace(/\r\n/g, "\n");
+    insertTextIntoField(el, normalizedText);
+  };
+
+  return insertText;
 };
